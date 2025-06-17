82821903957d943cde98f0d12a5f702dec7a8730
Bryan Sa <bsa@steamulo.com>
2025-06-17 14:06:26 +0200

    feat(ROUTE): création des premières routes + ajout de react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import logo from "./logo.svg"
 import "./App.css";
+import { Route, Routes, useLocation } from 'react-router-dom'
 
 function App() {
   return (
-   <div></div>
+   <Routes>
+     <Route path="/" element={<div></div>} />
+     <Route path="/about" element={<></>} />
+   </Routes>
   );
 }
 
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Page pour la HomePage
+import { useLocation } from "react-router-dom";
+
+
+const HomePage: React.FC = () => {
+  return (
+    <div>
+      {/*
+        Mettre la navbar
+      */}
+    </div>
+  );
+}
+export default HomePage;

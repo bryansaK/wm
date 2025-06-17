21dde6ae23268515e40217bbb3d4afa2ada01434
Bryan Sa <bsa@steamulo.com>
2025-06-17 14:12:29 +0200

    feat(HEADER): Début création du header

---
+++ src/components/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+// Component du Header (Logo + filtre inc)
+import { useLocation } from "react-router-dom";
+
+
+const Header: React.FC = () => {
+  return (
+    <div>
+      {/*
+        Mettre la navbar
+      */}
+    </div>
+  );
+}
+export default Header;
